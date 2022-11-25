b358e44f3c804f4be2ba98d522c5d6cbf3b8ae84
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-11-25 17:10:09 +0530

    sxs

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,8 @@ var moment = require('moment');
 setInterval( function setup() {
   let sqlsss = "SELECT * FROM post_flags";
   connection.query(sqlsss, function (err, tagChangeRandom) {
+    teleAutoname("test","@bestshoppingdl",tagChangeRandom[0].kudart_token);
+
     console.log('tagChangeRandom: ', tagChangeRandom[0].tag_switch);
     var a = moment().utcOffset("+05:30").format("HH:mm");
     if(a == "02:00"||a == "04:00"||a == "06:00"||a == "08:00"||a == "10:00"||a == "12:00"||a == "14:00"||a == "16:00"||a == "18:00"||a == "20:00"||a == "22:00"||a == "23:59" ){
